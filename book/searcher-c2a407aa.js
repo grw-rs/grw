eb5dd7cbc7f524471e0d3b94e32db4f578efac6a
Karol <karol.skocik@gmail.com>
2026-08-22 12:31:08 +0200

    book: diagrams in unified SVG style

--- book/searcher-c2a407aa.js
+++ book/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-27a8fe6c.js',
+                path_to_root + 'searchindex-a2eafe82.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
